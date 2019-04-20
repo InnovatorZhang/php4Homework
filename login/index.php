f4dc056f82547af08f0e786b6e84b41bfee706a6
InnovatorZhang <1070042265@qq.com>
2019-04-20 21:29:02 +0800

    加入登陆接口，成功登陆会返回一个token，以后每次访问都要带有符合token

--- login/index.php
+++ login/index.php
@@ -5,4 +5,38 @@
  * Date: 2019/4/20
  * Time: 17:47
  */
-echo 'sds';
+include(dirname(__FILE__)."/../utils/mysqlConnect.php");
+include(dirname(__FILE__)."/../utils/token.php");
+
+/**
+ * {
+ * "type":0,
+ * "content":{
+ * "username":"张中豪",
+ * "password":"zhagzhonghao"
+ * }
+ * }
+ */
+
+$postData = file_get_contents('php://input');
+
+//将json数据解析成数组
+$arrayData = json_decode($postData,true);
+//拿到用户名和密码
+if($arrayData['type'] == 0){
+    $username = $arrayData['content']['username'];
+    $password = $arrayData['content']['password'];
+}else{
+    other_encode('type属性错误');
+}
+
+//当用户登陆时更新token，以后每次请求必须带有token访问
+create_unique($pdo,$username);
+
+$sql = $pdo->prepare("SELECT id,username,password,nickname,avatar,phonenumber,sex,birthday,school,token FROM user WHERE username = ? AND password = ?");
+$sql->execute(array($username, $password));
+if ($row = $sql->fetch(PDO::FETCH_NAMED)) {
+    success_encode($row);
+} else {
+    other_encode("账号密码错误");
+}
--- utils/token.php
+++ utils/token.php
@@ -3,20 +3,20 @@ include("jsonWrapper.php");
 
 function checkToken(PDO $pdo, $token)
 {
-    $sql = $pdo->prepare("SELECT `id` FROM `person` WHERE `token` = ?");
+    $sql = $pdo->prepare("SELECT `username` FROM `user` WHERE `token` = ?");
     $sql->execute(array($token));
     if ($row = $sql->fetch(PDO::FETCH_NAMED)) {
-        return $row["id"];
+        return $row["username"];
     } else {
-        other_encode(401, "错误的token:{$token}");
+        other_encode("错误的token:{$token}");
         return null;
     }
 }
-function create_unique(PDO $pdo, $id)
+function create_unique(PDO $pdo, $username)
 {
     $data = $_SERVER['HTTP_USER_AGENT'] . $_SERVER['REMOTE_ADDR'].time().rand();
     $data = sha1($data);
-    $pdo->prepare("UPDATE person SET `token` = ? WHERE `id` = ?")->execute(array($data, $id));
+    $pdo->prepare("UPDATE user SET `token` = ? WHERE `username` = ?")->execute(array($data, $username));
     return $data;
 }
 
