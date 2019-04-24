e02b66076fbc14c03a6bb3d7fb9b1fdaf25bcc6d
InnovatorZhang <1070042265@qq.com>
2019-04-24 08:59:20 +0800

    增加数据库表项

--- login/index.php
+++ login/index.php
@@ -33,7 +33,7 @@ if($arrayData['type'] == 0){
 //当用户登陆时更新token，以后每次请求必须带有token访问
 create_unique($pdo,$username);
 
-$sql = $pdo->prepare("SELECT id,username,password,nickname,avatar,phonenumber,sex,birthday,school,token FROM user WHERE username = ? AND password = ?");
+$sql = $pdo->prepare("SELECT id,username,password,nickname,avatar,punchsign,phonenumber,gender,birthday,school,token FROM user WHERE username = ? AND password = ?");
 $sql->execute(array($username, $password));
 if ($row = $sql->fetch(PDO::FETCH_NAMED)) {
     success_encode($row);
